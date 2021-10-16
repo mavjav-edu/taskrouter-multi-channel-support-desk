6e9e1b6bf769bb41d26e4691fd0de9a5c017801f
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2021-10-15 21:15:51 -0700

    integrate keyring for local testing

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ from twilio import twiml
 from twilio.rest import Client
 from twilio.rest.taskrouter.v1.workspace.activity import ActivityInstance
 from twilio.rest.taskrouter.v1.workspace.worker import WorkerInstance
-from werkzeug.datastructures import auth_property
+import keyring
 
 
-ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', '')
-AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', '')
-SUPPORT_DESK_NUMBER = os.environ.get('SUPPORT_DESK_NUMBER', '')
-WORKSPACE_SID = os.environ.get('WORKSPACE_SID', '')
-WORKFLOW_SID = os.environ.get('WORKFLOW_SID', '')
+ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', keyring.get_password('twilio', 'TWILIO_ACCOUNT_SID'))
+AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', keyring.get_password('twilio', 'TWILIO_AUTH_TOKEN'))
+SUPPORT_DESK_NUMBER = os.environ.get('SUPPORT_DESK_NUMBER', keyring.get_password('twilio', 'SUPPORT_DESK_NUMBER'))
+WORKSPACE_SID = os.environ.get('WORKSPACE_SID', keyring.get_password('twilio', 'WORKSPACE_SID'))
+WORKFLOW_SID = os.environ.get('WORKFLOW_SID', keyring.get_password('twilio', 'WORKFLOW_SID'))
 
+
+# To commit these to the key store in PowerShell, just run:
+# $vars=@("SUPPORT_DESK_NUMBER", "TWILIO_ACCOUNT_SID", "TWILIO_AUTH_TOKEN", "WORKFLOW_SID", "WORKSPACE_SID"); foreach($var in $vars){pyenv exec keyring set twilio $var}
 
 XML_CONTENT_TYPE = 'application/xml'
 JSON_CONTENT_TYPE = 'application/json'
@@ -35,6 +38,7 @@ def call():
 
 @app.route('/assign', methods=['POST'])
 def assign():
+    global client
     task_attrs = json.loads(request.form['TaskAttributes'])
     if 'training' in task_attrs and task_attrs['training'] == 'sms':
         number = json.loads(request.form['WorkerAttributes'])['phone_number']
@@ -56,6 +60,7 @@ def assign():
 
 @app.route('/message', methods=['POST'])
 def message():
+    global client
     # check if one of our workers is completing a task
     if request.form['Body'] == 'DONE':
         from_number = request.form['From']
@@ -85,7 +90,7 @@ def message():
     tasks = client.taskrouter.workspaces(WORKSPACE_SID).tasks.create(
         workflowSid=WORKFLOW_SID, taskChannel='voice', attributes=task_attributes)
         
-    # Print the tasks in an easy to read format
+    # Log the tasks to debug output in an easy to read format
     print(json.dumps(tasks.__dict__, indent=2))
 
     r = twiml.Response()
